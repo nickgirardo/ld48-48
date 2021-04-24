2cfb877895ad40d1abda19244386ffce94d574f2
Nick Girardo <nickgirardo@gmail.com>
2021-04-24 16:12:53 -0400

    Keyboard util

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 
+import { init } from './util/keyboard';
+
 const canvas = document.querySelector('canvas');
 
 console.log(canvas);
+
+// Set up keyboard listeners
+init();
---
+++ src/util/keyboard.ts
@@ -0,0 +1,55 @@
+// Keyboard handler util
+// Note that the actual state of the keyboard isn't being exported yet
+
+enum Keys {
+    UP = 'up',
+    DOWN = 'down',
+    LEFT = 'left',
+    RIGHT = 'right',
+    JUMP = 'jump',
+    SWING = 'swing',
+    FIRE = 'fire',
+};
+
+const keysDown: { [key in Keys]: boolean } = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+    jump: false,
+    swing: false,
+    fire: false,
+};
+
+let keyMap = new Map<number, Keys>();
+
+keyMap.set(87, Keys.UP);
+keyMap.set(83, Keys.DOWN);
+keyMap.set(65, Keys.LEFT);
+keyMap.set(68, Keys.RIGHT);
+keyMap.set(74, Keys.JUMP);
+keyMap.set(75, Keys.SWING);
+keyMap.set(76, Keys.FIRE);
+
+// Set up keyboard event listeners
+export const init = () => {
+    document.addEventListener('keydown', event => {
+        const key = keyMap.get(event.keyCode);
+
+        // If we don't find the key in the map we don't care about it
+        if (!key)
+            return;
+
+        keysDown[key] = true;
+    });
+
+    document.addEventListener('keyup', event => {
+        const key = keyMap.get(event.keyCode);
+
+        // If we don't find the key in the map we don't care about it
+        if (!key)
+            return;
+
+        keysDown[key] = false;
+    });
+};
