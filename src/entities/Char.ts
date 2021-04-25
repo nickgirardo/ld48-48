29766ee4868d95118c3138c32dcd7ac42491a8cf
Nick Girardo <nickgirardo@gmail.com>
2021-04-25 17:19:50 -0400

    Added anim states to char

--- src/entities/Char.ts
+++ src/entities/Char.ts
@@ -6,8 +6,28 @@ import * as Vec2 from '../Vec2';
 
 import { CollisionBoundry, checkOverlap } from '../CollisionBoundry';
 
+enum CharAnim {
+    IDLE,
+    RUNNING,
+    JUMPING,
+    FALLING,
+    SWINGING,
+    SLING_SHOT,
+    NAIL_GUN,
+    HURT,
+    DEAD,
+}
+
+enum Facing {
+    LEFT,
+    RIGHT,
+}
+
 export class Char implements Entity {
+    kind = EntityTypes.CHAR;
     scene: Scene | undefined;
+    animState: CharAnim = CharAnim.IDLE;
+
     pos: Vec2.Vec2 = [0, 0];
     size: Vec2.Vec2 = [48, 92];
     vel: Vec2.Vec2 = [0, 0];
@@ -22,8 +42,10 @@ export class Char implements Entity {
     // Counted down on each frame
     stun: number = 0;
 
-    // TODO this might be helpful
-    kind = EntityTypes.CHAR;
+    facing: Facing = Facing.RIGHT;
+    lastJumpFrame: number = 0;
+    lastGroundedFrame: number = -1;
+
 
     render() {
         window.renderer.debug(this.getCollisionBounds(), 'darkgreen');
@@ -74,18 +96,49 @@ export class Char implements Entity {
         const fGravityJumping: Vec2.Vec2 = [0, 1.22];
         this.vel = Vec2.add(this.vel, keysDown[Keys.JUMP] ? fGravityJumping : fGravity);
 
+        const grounded: boolean = ground.getPosYClearance(this.getCollisionBounds()) === 0;
+
+        if (grounded) {
+            if (this.lastGroundedFrame < this.lastJumpFrame)
+                this.lastGroundedFrame = window.frame;
+
+            // This will be set later if the character is running or something
+            this.animState = CharAnim.IDLE;
+        }
+
+        if (!grounded) {
+            if (this.lastGroundedFrame > this.lastJumpFrame)
+                this.animState = CharAnim.FALLING;
+        }
+
         // The player can only move or jump if not stunned
         if (!this.stun) {
-            if (keysDown[Keys.LEFT])
+            if (keysDown[Keys.LEFT] && !keysDown[Keys.RIGHT]) {
                 this.vel = Vec2.add(this.vel, [-this.speed, 0]);
-            if (keysDown[Keys.RIGHT])
+                if (grounded) {
+                    this.animState = CharAnim.RUNNING;
+                    this.facing = Facing.LEFT;
+                }
+            }
+            if (keysDown[Keys.RIGHT] && !keysDown[Keys.LEFT]) {
                 this.vel = Vec2.add(this.vel, [this.speed, 0]);
+                if (grounded) {
+                    this.animState = CharAnim.RUNNING;
+                    this.facing = Facing.RIGHT;
+                }
+            }
 
             // If the character is grounded they can jump
             const fJump: number = -20;
-            const grounded: boolean = ground.getPosYClearance(this.getCollisionBounds()) === 0;
-            if (grounded && keysDown[Keys.JUMP])
+            if (grounded && keysDown[Keys.JUMP]) {
+                this.lastJumpFrame = window.frame;
+                this.animState = CharAnim.JUMPING;
                 this.vel[1] = Math.min(this.vel[1], fJump);
+            }
+        }
+
+        if (this.stun) {
+            this.animState = CharAnim.HURT;
         }
 
         // Friction
