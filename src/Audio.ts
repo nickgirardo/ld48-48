230a443123012070dee7486583a9475802602aa0
Nick Girardo <nickgirardo@gmail.com>
2021-04-25 23:11:10 -0400

    Audio to typescript

--- src/Audio.ts
+++ src/Audio.ts
@@ -1,1 +1,5 @@
+// TODO not a fan of slapping down the ts-ignore, but that seems like the best
+// way to get this working in our time frame
+
+// @ts-ignore
 export const jumpSound = new SoundEffect("5EoyNVSymuxD8s7HP1ixqdaCn5uVGEgwQ3kJBR7bSoApFQzm7E4zZPW2EcXm3jmNdTtTPeDuvwjY8z4exqaXz3NGBHRKBx3igYfBBMRBxDALhBSvzkF6VE2Pv").generate();
